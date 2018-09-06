ddaa7030f6fd427f69177f94dcace9786ac4fc24
Johan Beckers <jb@eenwereldmetlef.be>
2018-09-06 17:19:42 +0200

    Add Email, Phone and Password input types

--- Components.js
+++ Components.js
@@ -1,14 +1,17 @@
 import { Library } from './Library'
 
 const library = new Library([
-  [ 'textarea','./components/Textarea' ],
-  [ 'text', './components/Text'],
-  [ 'checkbox','./components/Checkbox']
+  ['textarea', './components/Textarea'],
+  ['text', './components/Text'],
+  ['password', './components/GenericInput'],
+  ['email', './components/GenericInput'],
+  ['phone', './components/GenericInput'],
+  ['checkbox', './components/Checkbox']
 ])
 
 // replace the paths with components and their config
 
-library.forEach((path,name)=> {
+library.forEach((path, name) => {
   library.set(name, {
     component: require(path).default,
     config: require(path).config
--- components/GenericInput.js
+++ components/GenericInput.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 import { Input } from 'reactstrap'
 
-const GenericInput = ({ bindInput, element, attributes })=> {
+const GenericInput = ({ bindInput, element, attributes }) => {
   const { name, type, attributes: elementAttributes } = element
-  return <Input type={type}  {...bindInput(name)} {...elementAttributes} {...attributes} />      
+  return (
+    <Input
+      type={type}
+      {...bindInput(name)}
+      {...elementAttributes}
+      {...attributes}
+    />
+  )
 }
 
+const config = [
+  {
+    name: 'name',
+    type: 'text',
+    label: 'Field name',
+    attributes: {
+      placeholder: 'Technical name for field'
+    },
+    validation: { required: true },
+    layout: { col: { md: '12' } }
+  },
+  {
+    name: 'attributes.placeholder',
+    type: 'text',
+    label: 'Placeholder',
+    layout: { col: { md: '12' } }
+  }
+]
+
 export default GenericInput
+export { config }
