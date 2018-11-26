30ac560dc4cf29809686503940dc72621aaf8164
Johan Beckers <jb@eenwereldmetlef.be>
2018-11-26 12:24:17 +0100

    Make infobox multilingual

--- components/InfoBox.js
+++ components/InfoBox.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Text } from 'meteor/lef:utils'
 import { translatorText } from '../translator'
 
+import { upperCase } from 'lodash'
+
 const InfoBoxComponent = props => {
   const { translator, bindInput, element, attributes: propsAttributes } = props
   const { attributes: elementAttributes } = element
@@ -12,20 +14,30 @@ const InfoBoxComponent = props => {
   )
 }
 
-const config = ({ translator, model }) => [
-  {
-    name: 'label',
-    type: 'textarea',
-    key: 'infobox',
-    label: 'Text contents',
-    attributes: {
-      rows: 5
-    },
-    layout: {
-      col: { xs: 12 }
-    }
+const config = ({ translator, model }) => {
+  const { languages } = translator || {}
+  if (languages) {
+    return languages.map(language => ({
+      key: 'infobox.' + language,
+      name: 'label.' + language,
+      type: 'textarea',
+      label: upperCase(language),
+      layout: { col: { xs: 12 } },
+      attributes: { rows: 4 }
+    }))
+  } else {
+    return [
+      {
+        name: 'label',
+        type: 'textarea',
+        key: 'infobox',
+        label: 'Text contents',
+        attributes: { rows: 5 },
+        layout: { col: { xs: 12 } }
+      }
+    ]
   }
-]
+}
 
 export default InfoBoxComponent
 export { config }
