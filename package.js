3b62a277725073ad2f91bfe87dc005004cd44bc6
johanlef <jb@eenwereldmetlef.be>
2019-01-10 11:47:28 +0100

    Upload component allows specification of thumbnails

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lef:forms2',
-  version: '1.2.10',
+  version: '1.2.11',
   // Brief, one-line summary of the package.
   summary: 'Easy, configuration based form composition',
   git: '',
