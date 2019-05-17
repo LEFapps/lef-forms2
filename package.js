0d988fdd9389efedaaea4e8573c24768e7965d47
Tomas Vink <tv@eenwereldmetlef.be>
2019-05-17 11:28:51 +0200

    forms update to 1.4.6

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lef:forms2',
-  version: '1.4.0',
+  version: '1.4.6',
   summary: 'Easy, configuration based form composition',
   git: '',
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.onUse(function (api) {
   Npm.depends({
     'react-dom': '16.5.1',
     'react-reformed': '2.0.0',
-    '@lefapps/forms': '1.4.5',
+    '@lefapps/forms': '1.4.6',
     '@lefapps/translations': '3.0.0'
   })
   api.mainModule('index.js', 'client')
